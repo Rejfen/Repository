8086f30f76b073b0580e4216eae197130ac8cbc7
Sebastian Banaś <sebastianbanas4@gmail.com>
2022-11-08 10:29:27 +0100

    added content to index.php and style.css

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php 
     require __DIR__ . '/vendor/autoload.php';
 
@@ -6,19 +5,38 @@
     $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
     $log->warning('Foo');
 ?>
-
 <!DOCTYPE html>
 <html lang="pl">
 <head>
+    <link rel="stylesheet" href="style.css">
     <title>test</title>
 </head>
 <body>
-    <?php
-        echo "test";
-
-
-
-
-    ?>
+    <div class="border">
+        <form>
+            <input type="text" name="number">
+            <input type="submit" value="Submit">
+        </form>
+        <br />
+        <br />
+        <div class="list">
+            <table>
+                <ul>
+                    <li>teśt</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
